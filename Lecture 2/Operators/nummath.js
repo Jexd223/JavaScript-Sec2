11865a313ad3738c29c6d0b0e1288c1a8aaa5ec7
Jexd223 <s6606021611083@email.com>
2024-12-03 23:34:47 +0700

    Lecture 2 up v2

---
+++ Lecture 2/Operators/nummath.js
@@ -0,0 +1,16 @@
+let num = 100;
+
+console.log("********Basic Math********")
+console.log(num + 25);
+console.log(num - 100);
+console.log(num * 100);
+console.log(num / 1500);
+
+console.log("********Addittional Mathenatical Operations********");
+console.log(num % 1500);
+console.log(++num);
+console.log(num++);
+
+console.log("********Using the Math Object********");
+console.log(Math.PI);
+console.log(Math.sqrt(num));
