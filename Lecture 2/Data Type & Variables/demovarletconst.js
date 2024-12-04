8042bd3f80bd91880779d1497629697012b057ef
Jexd223 <s6606021611083@email.kmutnb.ac.th>
2024-12-04 13:52:48 +0700

    Up Func

--- Lecture 2/Data Type & Variables/demovarletconst.js
+++ Lecture 2/Data Type & Variables/demovarletconst.js
@@ -8,6 +8,8 @@ if (true){
     let world = "Hello World";
     console.log(world);
 }
+//console.log(world)
 
 const fixval = "Fix Value";
 console.log(fixval);
+//fixval = "tes assigne value to const"
---
+++ Lecture 2/Function/arrowf-sample.js
@@ -0,0 +1,17 @@
+const sum = (firstName,secondParam) => {
+    return firstName + secondParam;
+}
+console.log(sum(2,5));
+
+const printHello = () => {
+    console.log("Hello");
+}
+printHello();
+
+const checkWeight = (weight) => {
+    console.log('Baffafe weight : ${weight} kilogram');
+}
+checkWeight(25);
+
+const multiply = (a,b) => a*b;
+console.log(multiply(2,30));
