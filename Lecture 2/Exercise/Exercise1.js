5eefda7c39030592903565c5150555185084ff1e
Jexd223 <s6606021611083@email.kmutnb.ac.th>
2024-12-26 08:09:39 +0700

    Exxs

--- Lecture 2/Exercise/Exercise1.js
+++ Lecture 2/Exercise/Exercise1.js
@@ -1,4 +1,8 @@
-function findPerimert(length,width){
-    return 2 * (length + width);
+function findPerimeter(length, width) {
+    if (Number.isNaN(Number.parseFloat(length)) || Number.isNaN(Number.parseFloat(width))) {
+        return 0;
+    }
+    return 2 * (parseFloat(length) + parseFloat(width));
 }
-console.log(findPerimert(10,32));
+
+console.log(findPerimeter("10, 32")); // Correct output: 84
