75e2c4ac84e1110eda32d235f0ff2d9679407a45
Jexd223 <s6606021611083@email.kmutnb.ac.th>
2025-01-09 08:14:19 +0700

    โ

--- Practice/1.js
+++ Practice/1.js
@@ -1,13 +1,14 @@
 let values1 = ['Apple', 1 , false];
-let values2 = ['Fries', 2 , true];
+let values2 = ['Fries', 2 , true,'Mars'];
 let values3 = ['Mars', 9 , 'Apple'];
+let values4 = ['Mars',2] 
 
 let commonElement = [];
-let newValues = values1.concat(values2,values3);
+let newValues = values1.concat(values2,values3,values4);
 
 for (let i = 0; i < newValues.length; i++) {
     for (let j = i + 1; j < newValues.length; j++) {
-        if (newValues[i] === newValues[j] ) {
+        if (newValues[i] === newValues[j] && !commonElement.includes(newValues[i])) {
             commonElement.push(newValues[i]);
         }
     }
